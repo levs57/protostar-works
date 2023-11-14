6c394810ec5d2e90ee9b818e996ce7d10fa5a0a5
Lev <0xdeadfae@gmail.com>
2023-11-14 11:52:22 +0300

    some progress

--- src/gadgets/lookup.rs
+++ src/gadgets/lookup.rs
@@ -7,17 +7,59 @@
 
 use std::iter::repeat_with;
 
-use ff::PrimeField;
+use ff::{PrimeField, BatchInvert};
 use itertools::Itertools;
 use num_bigint::BigUint;
 
 use crate::{constraint_system::Variable, utils::field_precomp::FieldUtils, circuit::{Build, Circuit, ExternalValue, Advice}, gate::Gatebb};
 
-// pub struct Lookup<F: PrimeField + FieldUtils> {
-//     values: Vec<F>,
-//     table: Vec<Variable>,
-//     access_counts: Vec<Variable>,
-// }
+/// Outputs a product of vector elements and products skipping a single element.
+pub fn montgomery<F: PrimeField+FieldUtils>(v: &[F]) -> (F, Vec<F>) {
+    if v.len() == 0 {return (F::ONE, vec![])}
+
+    let mut left = vec![F::ONE];
+    let mut right = vec![F::ONE];
+
+    let l = v.len();
+    for i in 0..l-1 {
+        let last = left[i];
+        left.push(last*v[i]);
+    }
+    for i in 0..l-1 {
+        let last = right[i];
+        right.push(last*v[l-i-1]);
+    }
+
+    let prod = left[l-1]*v[l-1];
+
+    let mut ret = vec![];
+
+    for i in 0..l-1 {
+        ret.push(left[i]*right[l-i-1])
+    }
+
+    (prod, ret)
+}
+
+/// Parses input as a, c, vals[0], ... vals[k-1] and constrains a = \sum 1/(vals[i]-c)
+/// Assumes denominators are nonzero (which always happens provided c is a random challenge); unsound otherwise.
+pub fn sum_of_fractions<'a, F:PrimeField+FieldUtils> (args: &[F], k: usize) -> F {
+    let (tmp, vals) = args.split_at(2);
+    assert_eq!(vals.len(), k);
+    let (a, c) = (tmp[0], tmp[1]);
+    let (prod, skips) = montgomery(vals.iter().map(|t|*t-c));
+    todo!();
+}
+
+pub fn sum_of_fractions_flat_gadget<'a, F: PrimeField+FieldUtils>(
+    circuit: &mut Circuit<'a, F, Gatebb<'a, F>, Build>,
+    vars: Vec<Variable>,
+    challenge: Variable
+    ) -> Variable {
+        assert!(vars.len()>0);
+        vars[0]        
+    }
+
 
 /// 
 pub trait Lookup<'a, F: PrimeField+FieldUtils> {
@@ -84,15 +126,14 @@ impl<'a, F: PrimeField+FieldUtils> Lookup<'a, F> for RangeLookup<F> {
             let mut ret = vec![0; range];
             for var in vars{
                 let var = BigUint::from_bytes_le(var.to_repr().as_ref());
-                assert!(var<range.into(), "Error: lookup value out of range.");
-                let i = var.to_u32_digits()[0] as usize;
+                assert!(var < range.into(), "Error: lookup value out of range.");
+                let i = var.to_u64_digits()[0] as usize;
                 ret[i]+=1;
             }
             ret.into_iter().map(|x|F::from(x)).collect()
         });
         let _access_counts = circuit.advice(access_round, compute_accesses, vars, vec![]);
         // Allocate challenge.
-
         todo!("CONSTRAIN STUFF");
 
         challenge

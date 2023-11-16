4b2fc3030b553c57cef84c0d365e63fa3cb085e1
Amir <retnevila@gmail.com>
2023-11-16 23:42:21 +0300

    please compile

--- src/folding/prover.rs
+++ src/folding/prover.rs
@@ -1,10 +1,3 @@
-// given two relaxed instances A, B (relaxed here just means a single additional error value for major gate) compute majgate(A+t(B-A))
-// compute each constraint on A+t(B-A)
-// step 1: evaluate each constr of deg d on t=0, t=1, ..., t=d // probably makes sense to find for each witness element the max degree of a constraint it participates in
-// step 2: use my magic function to obtain values of the majgate in t=0 ... t = D+log_ceil(m)
-// ?????
-// step 3: folding
-
 use std::collections::HashMap;
 
 use ff::PrimeField;
@@ -43,22 +36,22 @@ pub fn constraint_multieval<'c, F: PrimeField, G: Gate<'c, F>>(constraint: &Cons
     }).flatten().collect()
 }
 
-pub fn fold<'c, F: PrimeField, G: Gate<'c, F>>(a: RelaxedInstance<'c, F, G>, b: RelaxedInstance<'c, F, G>) {
-    // WARNING: it is essential to check that we're folding the same circuits
-    // TODO (aliventer): assert_eq!(instance_a.cs, instance_b.cs);
+// pub fn fold<'c, F: PrimeField, G: Gate<'c, F>>(a: RelaxedInstance<'c, F, G>, b: RelaxedInstance<'c, F, G>) {
+//     // WARNING: it is essential to check that we're folding the same circuits
+//     // TODO (aliventer): assert_eq!(instance_a.cs, instance_b.cs);
 
-    // step 1: for each variable v, precompute A.v + t (B.v - A.v) for t = 0 .. max constraint degree of v (inclusive)
-    let mut line_evaluations: HashMap<Variable, Vec<F>> = HashMap::new();
-    for var in a.wtns.cs.iter_variables() {
-        let av = a.wtns.getvar(var);
-        let bv = b.wtns.getvar(var);
-        let num_evals = a.wtns.cs.env[var].max_constraint_degree + 1;
+//     // step 1: for each variable v, precompute A.v + t (B.v - A.v) for t = 0 .. max constraint degree of v (inclusive)
+//     let mut line_evaluations: HashMap<Variable, Vec<F>> = HashMap::new();
+//     for var in a.wtns.cs.iter_variables() {
+//         let av = a.wtns.getvar(var);
+//         let bv = b.wtns.getvar(var);
+//         let num_evals = a.wtns.cs.env[var].max_constraint_degree + 1;
 
-        line_evaluations.insert(var, DiscreteRay::new(av, bv - av).take(num_evals).collect());
-    }
+//         line_evaluations.insert(var, DiscreteRay::new(av, bv - av).take(num_evals).collect());
+//     }
 
-    // step 2: evaluate each constraint f on A + t (B - A) for t = 0 .. deg f (inclusive)
-    for constr in a.wtns.cs.iter_constraints() {
-        let result = constraint_multieval(constr, &line_evaluations);
-    }
-}
+//     // step 2: evaluate each constraint f on A + t (B - A) for t = 0 .. deg f (inclusive)
+//     for constr in a.wtns.cs.iter_constraints() {
+//         let result = constraint_multieval(constr, &line_evaluations);
+//     }
+// }
